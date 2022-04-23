983d956d9216ae687ecdcabdcbff966ed46ad94e
SapumalPriyashantha <sapumalpriyashantha2000@gmail.com>
2022-04-23 11:50:40 +0530

    completed Tic Toc Toe game

--- assets/js/js.js
+++ assets/js/js.js
@@ -4,38 +4,38 @@ cells = Array.from(cells)
 let currentPlayer = "X"
 
 let winningCombinations = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 
-function checkForWinner(){
-    winningCombinations.forEach(function(combination){
+function checkForWinner() {
+    winningCombinations.forEach(function (combination) {
         let check = combination.every(idx => cells[idx].innerText.trim() == currentPlayer)
-        if(check){
+        if (check) {
             highlightCells(combination)
         }
     })
 }
 
-function highlightCells(combination){
-    combination.forEach(function(idx){
+function highlightCells(combination) {
+    combination.forEach(function (idx) {
         cells[idx].classList.add("highlight")
-        let win = "winner is "+currentPlayer
+        let win = "winner is " + currentPlayer
         $("#winner").text(win);
-        $("#winner").css("font-size","25px");
+
 
     })
 }
 
-cells.forEach(function(cell){
-    cell.addEventListener('click', function(){
-        if(cell.innerText.trim() != "") return
+cells.forEach(function (cell) {
+    cell.addEventListener('click', function () {
+        if (cell.innerText.trim() != "") return
         cell.innerText = currentPlayer
         checkForWinner()
         currentPlayer = currentPlayer == "X" ? "O" : "X"
